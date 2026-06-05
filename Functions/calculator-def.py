0ba6ca5d8dc5235540cdc9a008f03b7fd2420711
chellojuramu <chelloju@outlook.com>
2026-06-05 15:50:31 +0530

    feat: add calculator scripts and hello world program with IDE ignore configuration
    	new file:   .idea/.gitignore
    	new file:   Functions/calculator-def.py
    	new file:   Functions/calculator.py
    	new file:   hello.py

---
+++ Functions/calculator-def.py
@@ -0,0 +1,26 @@
+num1 = 10
+num2 = 5
+
+def addition():
+    add = num1 + num2
+    print(f"Addition result is: {add}")
+
+def substraction():
+    sub = num1 - num2
+    print(f"substraction result is: {sub}")
+
+def multiplication():
+    mul = num1 * num2
+    print(f"multiplication result is: {mul}")
+
+def division():
+    div = num1 / num2
+    print(f"division result is: {div}")
+
+
+addition()
+substraction()
+multiplication()
+division()
+
+
---
+++ Functions/calculator.py
@@ -0,0 +1,7 @@
+num1 = 10
+num2 = 5
+additon = num1 + num2
+print("value of addition" + str(additon))
+
+subtraction = num1 - num2
+print(subtraction)
